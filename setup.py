c9f6407d46a1ac268e5d968189458d1b7f3f9680
calingeorgediea <calin.george.diea@gmail.com>
2024-04-13 00:03:28 +0300

    Lets go

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A SHORT MVP for self marathon.',
+    author='Calin Diea',
+    license='',
+)
