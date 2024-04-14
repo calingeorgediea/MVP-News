74dd36909a742c5cea8ca60837b04f23d4e9384f
calingeorgediea <calin.george.diea@gmail.com>
2024-04-14 21:00:17 +0300

    choose number of topics based on coherence.

--- models/app_model_01.py
+++ models/app_model_01.py
@@ -2,13 +2,13 @@ import os
 import sys
 import spacy
 from gensim import corpora, models
+from gensim.models import CoherenceModel
 from spacy.lang.ro.stop_words import STOP_WORDS
 import pandas as pd
 nlp = spacy.load("ro_core_news_lg")
-def text_pipeline(data_file, num_topics=10, passes=15):
-    # Load the spaCy model for Romanian
-    
 
+
+def text_pipeline(data_file, passes=15, start=2, limit=20):
     # Load your text data from CSV
     texts = pd.read_csv(data_file)
 
@@ -26,12 +26,24 @@ def text_pipeline(data_file, num_topics=10, passes=15):
     # Create corpus of document vectors
     corpus = [dictionary.doc2bow(text) for text in processed_texts]
 
-    # Train the LDA model
-    lda_model = models.LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=passes)
+    # Calculate coherence scores for different number of topics
+    coherence_scores = []
+    for num_topics in range(start, limit):
+        lda_model = models.LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=passes)
+        coherence_model_lda = CoherenceModel(model=lda_model, texts=processed_texts, dictionary=dictionary, coherence='c_v')
+        coherence_score = coherence_model_lda.get_coherence()
+        coherence_scores.append((num_topics, coherence_score))
+
+    # Choose the number of topics with the highest coherence score
+    best_num_topics, best_coherence_score = max(coherence_scores, key=lambda x: x[1])
+    print(f"Best Number of Topics: {best_num_topics} with Coherence Score: {best_coherence_score}")
+
+    # Train the final LDA model with the best number of topics
+    lda_model = models.LdaModel(corpus, num_topics=best_num_topics, id2word=dictionary, passes=passes)
 
     # Save the topics and their corresponding words to a single file
     with open(f"./output/{os.path.splitext(os.path.basename(data_file))[0]}_topics.txt", 'w') as f:
-        for idx, topic in lda_model.print_topics(num_topics):
+        for idx, topic in lda_model.print_topics(num_topics=best_num_topics):
             words = topic.split('+')
             topic_words = [word.split('*')[1].replace('"', '').strip() for word in words]
             topic_str = ', '.join(topic_words)
@@ -39,6 +51,7 @@ def text_pipeline(data_file, num_topics=10, passes=15):
             f.write(f"{topic_str}\n\n")
 
     print("Topics saved in a single .txt file")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
